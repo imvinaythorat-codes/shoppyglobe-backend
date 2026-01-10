e46e2e127fe11fdd1ad63c106a05259c3979d0a9
Vinay <work.vinaythorat@gmail.com>
2026-01-10 19:16:45 +0530

    Add MongoDB connection setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+// connect database
+connectDB();
+
+// test route
 app.get("/", (req, res) => {
   res.send("ShoppyGlobe Backend Running");
 });
@@ -10,4 +16,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-    
